4881c2679791d4f0c499fd4f9d74be598e4db00b
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2023-11-22 10:59:32 +0100

    Add Lesson 1 answers
    Changes to be committed:
    	new file:   lesson-01.sql

---
+++ lesson-01.sql
@@ -0,0 +1,14 @@
+-- 1. Find the title of each film 
+SELECT Title FROM movies;
+
+-- 2. Find the director of each film
+SELECT Director FROM movies;
+
+-- 3. Find the title and director of each film
+SELECT Title, Director FROM movies;
+
+-- 4. Find the title and year of each film
+SELECT Title, Year FROM movies;
+
+-- 5. Find all the information about each film
+SELECT * FROM movies;
